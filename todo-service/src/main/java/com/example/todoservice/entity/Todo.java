1825e6134ad5605e0bee40c4186a8031105ebdf0
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2022-04-08 18:59:01 +0530

    [Todo.java] | build | Added todo entity

---
+++ todo-service/src/main/java/com/example/todoservice/entity/Todo.java
@@ -0,0 +1,40 @@
+package com.example.todoservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "todo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "todo_id", columnDefinition = "char(36)", nullable = false)
+    private UUID todoId;
+
+    @Column(name = "todo_title", nullable = false)
+    private String todoTitle;
+
+    @Column(name = "todo_description", nullable = false)
+    private String todoDescription;
+
+    @Column(name = "is_finished", nullable = false, columnDefinition = "boolean default false")
+    private boolean isFinished;
+
+}
